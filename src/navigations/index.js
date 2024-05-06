50ce7953848b0f686a2ce64cfb187100e717a70b
Nathalie <kantorazafim@gmail.com>
2024-05-06 15:17:01 +0300

    added screens folder and index.js file and install react-navigation

---
+++ src/navigations/index.js
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+
+export default function AppNavigation() {
+  return (
+    <View>
+      <Text>index</Text>
+    </View>
+  )
+}
